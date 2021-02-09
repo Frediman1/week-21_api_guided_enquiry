1cc732584093d03b3989501e6797c3ee17e54e74
Fredrick Evans <Fredrickevans21@gmail.com>
2021-02-09 16:25:38 -0500

    connected todo input to api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json());
 
 
 api.listen(3000, () => {
     console.log('API up and running!');
 });
+
+
+// post method 
+api.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('It works!');
+});
+
+
+
+
+
 
 // api.get('/', (req, res) => {
 //     console.log(req);
