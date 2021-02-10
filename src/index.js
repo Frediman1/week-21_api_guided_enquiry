082c7a132721df80a5d7aafeb393f685905c21c8
Fredrick Evans <Fredrickevans21@gmail.com>
2021-02-09 20:07:08 -0500

    Final with myql connected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('msql12');
+
+// Mysql Connections
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'todo'
+});
+// red flag for mysql connection
+try {
+    connection.connect();
+} catch (e) {
+    console.log('Oops. Connection to MySQL failed.');
+    console.log(e);
+}
+
+
+
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
@@ -14,7 +33,14 @@ api.listen(3000, () => {
 // post method 
 api.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('It works!');
+
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+        if (error) return res.json({ error: error });
+        connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+            if (error) return res.json({ error: error });
+            console.log(results);
+        });
+    });
 });
 
 
